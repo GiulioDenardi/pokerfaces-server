4319cdb03726f5697ba0ef2acbb7f64d3f262833
GiulioDenardi <giulio.denardi@outlook.com>
2016-12-03 15:55:26 -0200

    Initial

---
+++ src/main/java/com/pokerfaces/PokerfacesServerApplication.java
@@ -0,0 +1,12 @@
+package com.pokerfaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerfacesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerfacesServerApplication.class, args);
+	}
+}
